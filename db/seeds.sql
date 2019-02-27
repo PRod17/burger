62bd73aeca361f169937d1494a8e94432d0650ae
Paul Rodriguez <prodcapital@yahoo.com>
2019-02-27 16:29:08 -0700

    push

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Grass Fed Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
